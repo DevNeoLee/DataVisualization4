d51ee36b7c8150c8a3576f708418c1d5fa70a623
DevNeoLee <devneolee@gmail.com>
2020-04-17 17:42:22 -0700

    refactor data call logic to map chart

--- src/put_data.js
+++ src/put_data.js
@@ -6,8 +6,8 @@ import { mapChart } from './map_chart.js';
 //organize specific monthly data
 //call bar, pie, map chart drawing methods with organized data
 export const putData = (data, year = 10, month = 7) => {
-        
-    let monthlyData = [];
+
+    const monthlyData = [];
         for(let ele = 0; ele < data.length; ele ++ ) {
             if ((parseInt(data[ele]['REF_DATE'].slice(2, 4)) == year) &&
                 (parseInt(data[ele]['REF_DATE'].slice(5, 7)) == month))
@@ -16,7 +16,11 @@ export const putData = (data, year = 10, month = 7) => {
             }
             if (monthlyData.length == 12 ) { break }
         }
-    const sortedMonthlyData = monthlyData.sort(function(b, a){ return parseInt(a.VALUE) - parseInt(b.VALUE)});
+ 
+    mapChart(monthlyData); //call map chart with unsorted data
+  
+    const sortedMonthlyData = [...monthlyData]; //copy unsorted data
+    sortedMonthlyData.sort(function(b, a){ return parseInt(a.VALUE) - parseInt(b.VALUE)}); // sort data in order according to the value amount
     
     // calculate the total # of visitors to Canada on the month data
     const totalVisitor = sortedMonthlyData.reduce((a, b) => (a + parseInt(b.VALUE)), 0);
@@ -24,15 +28,14 @@ export const putData = (data, year = 10, month = 7) => {
     //call charts drawing with sorted monthly data
     barChart(sortedMonthlyData);
     pieChart(sortedMonthlyData, totalVisitor);
-    mapChart(sortedMonthlyData);
 
     // write site main title with specific visitor info for the month
-    d3.selectAll('.spanText').remove();
-    d3.select('span.info').append('g')
+    d3.selectAll('.info').remove();
+    d3.select('.infoWrap').append('g')
         .data(sortedMonthlyData)
         .append('text')
-        .attr('class', 'spanText')
-        .text(d => 'Total ' + totalVisitor + " Tourists Have Visited Canada" + " on " + d.REF_DATE );
+        .attr('class', 'info')
+        .text(d =>  totalVisitor + " Tourists Have Visited Canada" + " on " + d.REF_DATE );
 }
 
 
